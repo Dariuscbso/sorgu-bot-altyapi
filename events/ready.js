f836c18c5eccc6b21419dead236ac8f80d96f305
RetnoxXD <122722105+RetnoxXD@users.noreply.github.com>
2023-02-05 16:29:19 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ready',
+    execute(client) {
+        console.log(`${client.user.tag} Hazırlandı BOT SAHIBI Retnox#2728`)
+
+            client.user.setActivity(`Roblox`)
+    }
+}
